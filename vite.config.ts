e621239394c52c8c78e0c1fcf155ec2242f9a833
ayurveez <ayurveez2@gmail.com>
2025-12-15 02:15:37 +0530

    Final Fix: Base to ./

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // **CRITICAL FIX:** Corrected base path using the repository name 'updated'
-  base: '/updated/', 
+  base: './', 
   
   plugins: [react()],
   define: {
